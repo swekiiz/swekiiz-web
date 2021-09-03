2301fb5b918ee6c3f52f2e7466c35890954f924a
swekiiz <best_n.dol@hotmail.com>
2021-09-03 17:42:00 +0700

    add crypto images

---
+++ src/services/getImages.ts
@@ -0,0 +1,22 @@
+import BTC from 'assets/images/crypto/BTC.svg';
+import ETH from 'assets/images/crypto/ETH.svg';
+import ADA from 'assets/images/crypto/ADA.svg';
+import CAKE from 'assets/images/crypto/CAKE.svg';
+import MATIC from 'assets/images/crypto/MATIC.svg';
+import UNI from 'assets/images/crypto/UNI.svg';
+import USDC from 'assets/images/crypto/USDC.svg';
+import BNB from 'assets/images/crypto/BNB.svg';
+
+const cryptoIamges: { [token: string]: string | undefined } = {
+  ADA,
+  BNB,
+  BTC,
+  CAKE,
+  MATIC,
+  ETH,
+  UNI,
+  USDC,
+};
+
+export const getImg = (token: string): string | undefined =>
+  cryptoIamges[token];
